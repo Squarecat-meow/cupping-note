f1d8c5cbb96e403a8f11180da43c001ee199030d
Happy Blåhaj <245370322+happyblahaj@users.noreply.github.com>
2025-11-22 22:05:12 +0900

    feat: 원두 로스팅 정도 enum으로

--- packages/backend/src/schema/beans.ts
+++ packages/backend/src/schema/beans.ts
@@ -1,9 +1,11 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core'
+import { pgEnum, pgTable, serial, text } from 'drizzle-orm/pg-core'
+
+export const roastLevelEnum = pgEnum('roast_level', ['light', 'medium', 'medium_dark', 'dark'])
 
 export const beans = pgTable('beans', {
   id: serial('id').primaryKey(),
   name: text('name').notNull(),
   origin: text('origin').notNull(),
   variety: text('variety').notNull(),
-  roastLevel: text('roast_level').notNull(),
+  roastLevel: roastLevelEnum('roast_level').notNull(),
 })
