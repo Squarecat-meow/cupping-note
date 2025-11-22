c55f22301fd613bdcbaf1efc4a418a4512224e28
Squarecat-meow <yozumina@serafuku.moe>
2025-11-22 21:38:16 +0900

    Headless UI 제거 & Flowbite 라이브러리 추가

---
+++ packages/frontend/src/components/nav-tabs.tsx
@@ -0,0 +1,13 @@
+function NavTabs() {
+  return (
+    <nav>
+      <ul className="flex gap-2">
+        <li>레시피</li>
+        <li>커핑노트</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default NavTabs
+
